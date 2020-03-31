7f7fde2d5fa2403f9063abdad8f07814166e79c7
george kamau <flexkamau646@gmail.com>
2020-03-31 17:34:55 +0300

    added color to my background

--- src/app/quotes/Quotes.component.ts
+++ src/app/quotes/Quotes.component.ts
@@ -12,9 +12,9 @@ export class QuotesComponent implements OnInit, OnChanges {
   idx:number;
 
   quotes: Quote[] = [
-    new Quote("Cricket civilises people and creates good gentlemen. I want everyone to play cricket in Zimbabwe; I want ours to be a nation of gentlemen.", 'Robert Mugabe', new Date('2019, 02, 12'), 'john', 0, 0),
-    new Quote('Do not gain the world and lose your soul, wisdom is better than silver or gold...', 'Bob Marley', new Date('2019, 03, 12'), 'john', 0, 0),
-    new Quote("And I ask why am I black, they say I was born in sin, and shamed inequity. One of the main songs we used to sing in church makes me sick, 'love wash me and I shall be whiter than snow.", 'Peter Tosh', new Date('2019, 09, 12'), 'john', 0, 0),
+    new Quote("Cricket civilises people and creates good gentlemen. I want everyone to play cricket in Zimbabwe; I want ours to be a nation of gentlemen.", 'Robert Mugabe', new Date('2020, 03, 30'), 'john', 0, 0),
+    new Quote("Live as if you were to die tomorrow. Learn as if you were to live forever.", 'Mahatma Gandhi', new Date('2020, 31, 03'), 'George', 0, 0),
+    new Quote("And I ask why am I black, they say I was born in sin, and shamed inequity. One of the main songs we used to sing in church makes me sick, 'love wash me and I shall be whiter than snow.", 'Peter Tosh', new Date('2020, 03, 1'), 'john', 0, 0),
     new Quote("I freed a thousand slaves I could have freed a thousand more if only they knew they were slaves.", 'Harriet Tubman', new Date('2019, 10, 12'), 'john', 0, 0)
 
   ];
